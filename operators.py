24649c3cbc88227dc7fead54b793cf8131718a08
kumaraaravind <126396194+kumaraaravind@users.noreply.github.com>
2023-10-13 10:36:24 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,203 @@
+
+# Arthamatical operations
+#additon
+a = 20
+b = 5
+print(a+b)
+
+#subtraction
+a = 20
+b = 5
+print(a-b)
+
+#multipication
+
+a = 20
+b = 25
+print(a*b)
+
+#division
+a = 20
+b = 5
+print(a/b)
+
+#module
+a = 10
+b = 8
+print(a % b)
+
+#floordivison
+a = 20
+b = 5
+print(a//b)
+
+#comparison operators
+#==
+a = 10
+b = 10
+print(a == b)
+
+#==
+a = 10
+b = 2
+print(a == b)
+#!=
+a = 10
+b = 20
+print(a != b)
+#>
+a = 10
+b = 5
+print(a > b)
+#<
+a = 10
+b = 5
+print(a < b)
+#<=
+a = 10
+b = 5
+print(a <= b)
+#>=
+a = 10
+b = 5
+print(a >= b)
+
+# Assignment operators
+#+
+a = 10
+a += 10
+print(a)
+#-
+a = 15
+a -= 5
+print(a)
+#*
+a = 20
+a *= 3
+print(a)
+#/
+a = 20
+a /= 6
+print(a)
+#//=
+a = 10
+a //= 2
+print(a)
+#%
+a = 10
+a %= 2
+print(a)
+
+#Logical operator
+#and
+a = 10
+b = 8
+c = 5
+print(a > b)
+print(a > b and b > a)
+
+
+a = 10
+b = 8
+print(a < b)
+print(a > b and b < a)
+
+#Or
+a = 10
+b = 8
+print(a > b)
+print(a > b or b > a)
+a = 10
+b = 8
+print(a < b)
+print(a > b or b < a)
+
+#Not
+a = 10
+b = 8
+print(a > b)
+print(not(a > b or b > a))
+
+a = 10
+b = 8
+print(a < b)
+print(not(a > b or b < a))
+
+#bitwise operator
+#bitwise and opertor
+a = 10
+b = 10
+print(a & b)
+
+#bitwise or operator
+a = 10
+b = 10
+print(a | b)
+
+#bitwise xor operator
+a = 10
+b = 10
+print(a ^ b)
+
+#left shift operator
+a = 10
+b = 3
+print(a >> b)
+
+a = 3
+b = 10
+print(a >> b)
+
+#right shift opertor
+a = 10
+b = 3
+print(a << b)
+
+a = 3
+b = 10
+print(a << b)
+
+#check address variable
+a = 10
+print(id(a))
+
+a = 2
+b = 8
+print(id(a))
+print(id(b))
+
+#identify operator
+#is
+a = 10
+b = 10
+print(a is b)
+
+a = 4
+b = 8
+print(b is a)
+
+#is not
+a = 9
+b = 9
+print(a is not b)
+
+a = 2
+b = 8
+print(b is not a)
+
+#member operator
+#in
+a = "Aravind"
+print("a" in a)
+
+a = " kumar Aravind"
+print("Aravind" in a)
+
+#in not
+a = "Aravind"
+print("Aravind" not in a)
+
+a = "kumar Aravind"
+print("Aravind" not in a)
+
+
